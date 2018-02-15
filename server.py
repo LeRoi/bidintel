8e01fbe3281b0017ff5f3e67ef229a3dd2c562f3
LeRoi <kxhere@gmail.com>
2018-02-15 16:02:07 -0500

    Add server bones

---
+++ server.py
@@ -0,0 +1,15 @@
+import flask, json, os
+from flask import jsonify, Flask, request, render_template
+
+import lib.structure
+
+app = Flask(__name__)
+debug = True
+
+@app.route('/')
+def home():
+    return Response(render_template('main.html'))
+
+if __name__ == '__main_':
+    port = int(os.envion.get('PORT', Server.PORT))
+    app.run(host=Server.HOST, port=port, debug=debug)
